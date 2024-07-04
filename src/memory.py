61282b3544604bbb0b5813024e569ee751ed50b1
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-07-04 08:53:27 +0200

    fill memory with 0xff instead of 0 to see where memory is cleared added more kown functions

--- src/memory.py
+++ src/memory.py
@@ -3,12 +3,24 @@
 
 class Memory():
 
+    funcs = {
+       0x0038: "interrupt ROM()",
+       0x01e5: "interrupt2 ROM()",
+       0x01f3: "copy interrupt vectors to 4080",
+       0x01f8: "clear RAM from 4089 to 40ff",
+       0x02b1: "interrupt3 ROM()",
+       0x0410: "write 0x20 from 4100 to 417f",
+       0x04a9: "unknown_io()",
+       0x4083: "interrupt RAM()",
+       0x4086: "wait_for_kbd_or_printer()"
+    }
 
     def __init__(self, m):
-        self.m = m
+        self.m = m.memory
 
 
     def hexdump(self, address, length, icount):
+        nullpatt = "FF " * 16
         print(f"########### HEXDUMP 0x{address:x} - 0x{address+length:x} ####################################")
         print(f'icount {icount}')
         hexline = f"{address:04X} "
@@ -16,7 +28,7 @@ class Memory():
         count = 0
         prevempty = False
         for i in range(length):
-            byte  = self.m.memory[address + i]
+            byte  = self.m[address + i]
             hexline += f"{byte:02X} "
             if byte >=32 and byte < 122:
                 char += chr(byte)
@@ -24,7 +36,7 @@ class Memory():
                 char += "."
             count += 1
             if count == 16:
-                if hexline[5:] != "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ":
+                if hexline[5:] != nullpatt:
                     print(hexline, char)
                     prevempty = False
                 else:
@@ -42,22 +54,55 @@ class Memory():
         assert val >= 0 and val <= 255
 
         for i in range(start, end + 1):
-            self.m.memory[i] = val
+            self.m[i] = val
         print(f'set {end - start + 1} bytes of memory to {val}')
 
 
+    def writeu8(self, addr, val):
+        assert val >=0 and val <= 255
+        self.m[addr] = val
+
+
+    def getu8(self, address: int) -> int:
+        val = self.m[address]
+        assert val >= 0 and val <= 255
+        return val
+
+
+    def getu16(self, address):
+        return self.getu8(address) + (self.getu8(address+1) << 8)
+
+
     def getu32(self, address):
-        return (self.m.memory[address + 0] <<  0) + \
-               (self.m.memory[address + 1] <<  8) + \
-               (self.m.memory[address + 2] << 16) + \
-               (self.m.memory[address + 3] << 24)
+        lo = self.getu16(address)
+        hi = self.getu16(address+2)
+        return lo + (hi << 16)
 
 
-    def load(self, file, address):
+    def loadfile(self, file, address):
             fh = open(file, 'rb')
             block = list(fh.read())
             assert len(block) + address < 65535
             for i in range(len(block)):
-                self.m.memory[address + i] = block[i]
+                self.m[address + i] = block[i]
             print(f'loaded {len(block)} bytes from {file} at address {address}')
             fh.close()
+
+
+if __name__ == '__main__':
+
+    class Standin():
+        def __init__(self):
+            self.memory = [0 for i in range(65536)]
+            self.memory[0] = 0
+            self.memory[1] = 1
+            self.memory[2] = 2
+            self.memory[3] = 3
+
+    s = Standin()
+    mem = Memory(s)
+
+    assert mem.getu8(0) == 0
+    assert mem.getu8(1) == 1
+    assert mem.getu16(0) == 256
+    assert mem.getu32(0) == mem.getu16(0) + mem.getu16(2)*65536
--- src/single_stepping.py
+++ src/single_stepping.py
@@ -12,18 +12,24 @@ def main(args):
 
     # Clear all memory to 0
     mem = memory.Memory(m)
-    mem.set(0, 65535, 0)
+    mem.set(0, 65535, 0xff)
+
+    m.pc = args.start
 
     # Load ROMs to assumed addresses
-    mem.load("roms/IC25.BIN", 0x0000)
-    mem.load("roms/IC26.BIN", 0x0400)
-    mem.load("roms/IC27.BIN", 0x0800)
-    mem.load("roms/IC28.BIN", 0x0C00)
-    mem.load("roms/IC29.BIN", 0x1000)
-    mem.load("roms/IC30.BIN", 0x1400)
-    mem.load("roms/IC31.BIN", 0x1800)
-    mem.load("roms/IC32.BIN", 0x1C00)
+    mem.loadfile("roms/IC25.BIN", 0x0000)
+    mem.loadfile("roms/IC26.BIN", 0x0400)
+    mem.loadfile("roms/IC27.BIN", 0x0800)
+    mem.loadfile("roms/IC28.BIN", 0x0C00)
+    mem.loadfile("roms/IC29.BIN", 0x1000)
+    mem.loadfile("roms/IC30.BIN", 0x1400)
+    mem.loadfile("roms/IC31.BIN", 0x1800)
+    mem.loadfile("roms/IC32.BIN", 0x1C00)
 
+    # Setup interrupt routine (if nt starting at 0)
+    mem.writeu8(0x4083, 0xc3)
+    mem.writeu8(0x4084, 0xb1)
+    mem.writeu8(0x4085, 0x02)
 
     icount = 0
     mem.hexdump(0x2000, 0x10000 - 0x2000, icount) # dump RAM part of memory
@@ -34,6 +40,7 @@ def main(args):
             print(f'max instruction count reached, exiting ...')
             sys.exit()
 
+
         # Decode the instruction.
         MAX_INSTR_SIZE = 4
         instr = b.build_instr(m.pc, bytes(m.memory[m.pc:m.pc + MAX_INSTR_SIZE]))
@@ -41,18 +48,20 @@ def main(args):
         # Get and verbalise the instruction bytes.
         instr_bytes = bytes(m.memory[instr.addr:instr.addr + instr.size])
         instr_bytes = ' '.join(f'{b:02X}' for b in instr_bytes)
+        if m.pc in mem.funcs:
+            print(f'<<<< {mem.funcs[m.pc]} >>>>')
         print(f'{m.pc:04X} {instr_bytes:12} ; {instr_str:15} | SP={m.sp:04X}, A={m.a:02X} BC={m.bc:04X}, DE={m.de:04X}, HL={m.hl:04X}')
 
 
         if m.pc == args.poi: # PC of interest
             print('\n<<<<< pc of interest >>>>>\n')
 
-        if icount % 256 == 0 and not args.nodump:
+        if icount % args.dumpfreq == 0 and not args.nodump:
             mem.hexdump(0x2000, 0x10000 - 0x2000, icount) # dump RAM part of memory
 
         data = mem.getu32(m.pc)
-        if data == 0:
-            print(f'all zeroes at {m.pc:04x}, exiting ...')
+        if data == 0xffffffff:
+            print(f'all ones at {m.pc:04x}, exiting ...')
             sys.exit()
 
         # Limit runs to a single tick so each time we execute exactly one instruction.
@@ -67,7 +76,11 @@ if __name__ == "__main__":
                         type = int, default = 16380)
     parser.add_argument("-p", "--poi", help = "Point of interest (PC)",
                         type = int, default = 0x0d8a)
+    parser.add_argument("--dumpfreq", help = "Hexdump every N instruction",
+                        type = int, default = 256)
     parser.add_argument("-n", "--nodump", help = "Toggle hexdump", action='store_true')
+    parser.add_argument("-a", "--start", help = "start at specified address",
+                        type = int, default = 0)
 
     args = parser.parse_args()
 
