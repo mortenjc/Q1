31a3915f3fc11e85ada1caaa332d04748d3484cc
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-07-10 00:07:56 +0200

    added all in and out addresses seen so far

--- src/z80io.py
+++ src/z80io.py
@@ -1,7 +1,7 @@
 
 import sys
 
-# This is in a horrible state, still need to figure out how to use this
+#
 
 def isprintable(c):
     return c >= 0x20 and c <= 0x7D
@@ -13,16 +13,33 @@ class IO:
         self.outcb = {}
         self.keyincount = 0
         self.displaystr = ""
-        self.register_in_cb(0x01, self.handle_key_in)
+        self.register_in_cb( 0x01, self.handle_key_in)
+        self.register_out_cb(0x01, self.handle_key_out)
+
         self.register_out_cb(0x03, self.handle_display_out)
         self.register_out_cb(0x04, self.handle_display_out_ctrl)
-        self.register_in_cb(0x04, self.handle_display_in)
-        self.register_in_cb(0x05, self.handle_printer_in)
-        self.register_in_cb(0x0C, self.handle_uio_0c_in)
-        self.register_in_cb(0x1A, self.handle_uio_1a_in)
+        self.register_in_cb( 0x04, self.handle_display_in)
+
+        self.register_in_cb( 0x05, self.handle_printer_in)
+        self.register_out_cb(0x07, self.handle_printer_out_7)
+
+        self.register_out_cb(0x0a, self.handle_ciu_out_0a)
+        self.register_out_cb(0x0b, self.handle_ciu_out_0b)
+        self.register_in_cb( 0x0c, self.handle_ciu_in_0c)
+        self.register_out_cb(0x0c, self.handle_ciu_out_0c)
+
+        self.register_in_cb( 0x1a, self.handle_disk_in_1a)
+        self.register_out_cb(0x1a, self.handle_disk_out_1a)
+        self.register_out_cb(0x1b, self.handle_disk_out_1b)
 
 
     ### Functions for registering and handling IO
+
+    def register_out_cb(self, outaddr: int, outfunc):
+        self.outcb[outaddr] = outfunc
+
+    def register_in_cb(self, inaddr: int, infunc):
+        self.incb[inaddr] = infunc
 
     def handle_io_in(self, value) -> int:
         reg = value >> 8
@@ -41,16 +58,10 @@ class IO:
             #sys.exit()
 
 
-    def register_out_cb(self, outaddr: int, outfunc):
-        self.outcb[outaddr] = outfunc
-
-
-    def register_in_cb(self, inaddr: int, infunc):
-        self.incb[inaddr] = infunc
-
     ### Specific functions
 
     ### Display
+
     def handle_display_in(self) -> int:
         print('IO - display status: 0')
         return 0
@@ -63,6 +74,7 @@ class IO:
         else:
             print(f"IO - display out {val}")
 
+
     def handle_display_out_ctrl(self, val) -> str:
         if val == 0x05:
             desc = 'unblank, reset to (1,1)'
@@ -74,6 +86,7 @@ class IO:
 
 
     ### Keyboard
+
     def handle_key_in(self) -> int:
         self.keyincount += 1
         retval = 0
@@ -86,21 +99,63 @@ class IO:
         print(f'IO - key in (calls: {self.keyincount}): 0x{retval:02X}')
         return retval
 
+
+    def handle_key_out(self, val):
+        if val == 0x04:
+            desc = 'keyboard mode 2'
+        else:
+            desc = 'unknown'
+        print(f'IO - key out: {desc}')
+
+
     ### Printer
     def handle_printer_in(self) -> int:
         print(f'IO - printer in (status) : 0 (no errors)')
         return 0
 
 
-    ### Unknown IO1
-    def handle_uio_0c_in(self) -> int:
-        print('IO - unknown input (0x0C), return 0')
+    def handle_printer_out_7(self, val):
+        if val == 0xA0:
+            desc = 'reset printer, raise ribbon'
+        else:
+            desc = 'unknown command'
+        print(f'IO - printer out 7: {desc}')
+
+
+    ### CIU ?
+    def handle_ciu_out_0a(self, val):
+        print(f'IO - CIU out (data out) (0x{val:02X})')
+
+
+    def handle_ciu_out_0b(self, val):
+        print(f'IO - CIU out (control 1) (0x{val:02X})')
+
+
+    def handle_ciu_out_0c(self, val):
+        if val == 0x81:
+            desc = 'synchronous mode, master reset'
+        else:
+            desc = 'unspecified'
+        print(f'IO - CIU out (control 2) {desc} (0x{val:02X})')
+
+
+    def handle_ciu_in_0c(self):
+        print('IO - CIU in (control 2): 0x00')
         return 0
 
-    ### Unknown IO2
-    def handle_uio_1a_in(self) -> int:
-        print('IO - unknown input (0x1a), return 0')
+
+    ### Disk control (from Q1 Assembler p. 52)
+    def handle_disk_in_1a(self):
+        print('IO - in (disk status): 0x00')
         return 0
+
+
+    def handle_disk_out_1a(self, val):
+        print(f'IO - disk control 1: NOP? (0x{val:02X})')
+
+
+    def handle_disk_out_1b(self, val):
+        print(f'IO - disk control 2: NOP? (0x{val:02X})')
 
 
 if __name__ == '__main__':
